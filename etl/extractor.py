e12f73ce2d029b0984c45be66407d06d5093e005
Pedro Gonçalo <pedro.gonsalo939@gmail.com>
2026-03-07 15:02:02 -0300

    feat: add os arquivos iniciais do projeto, incluindo o arquivo .gitignore, os arquivos de dados, o extrator e os requisitos

---
+++ etl/extractor.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+class ExtractorExcel:
+    def __init__(self, file_path: str):
+        self.file_path = file_path
+
+    def extract(self) -> pd.DataFrame:
+        dataframe = pd.read_excel(self.file_path)
+        return dataframe
