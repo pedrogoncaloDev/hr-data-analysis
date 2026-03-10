d502060b1e28295461e4be0f3d85a651328df71d
PedroGoncaloGRV <pedro.henrique@grvsoftware.com.br>
2026-03-10 10:05:23 -0300

    feat: finalizando o transformer com function de conersão de dados booleanos

--- etl/transformer.py
+++ etl/transformer.py
@@ -8,10 +8,13 @@ logger = logging.getLogger(__name__)
 class DataFrameSchema:
 	date_columns: list[str] = field(default_factory=list)
 	numeric_columns: list[str] = field(default_factory=list)
+	data_booleans: list[str] = field(default_factory=list)
+
 
 class TransformerExcel:
 	def __init__(self, schemas: dict[str, DataFrameSchema]):
 		self.schemas = schemas
+
 
 	def transform(self, dataframes: dict[str, pd.DataFrame]) -> dict[str, pd.DataFrame]:
 		result = {}
@@ -26,10 +29,13 @@ class TransformerExcel:
 
 		return result
 
+
 	def _apply_schema(self, df: pd.DataFrame, schema: DataFrameSchema) -> pd.DataFrame:
 		df = self._convert_dates(df, schema.date_columns)
 		df = self._convert_numerics(df, schema.numeric_columns)
+		df = self._convert_booleans(df, schema.data_booleans)
 		return df
+
 
 	def _convert_dates(self, df: pd.DataFrame, columns: list[str]) -> pd.DataFrame:
 		for col in columns:
@@ -45,6 +51,7 @@ class TransformerExcel:
 			df[col] = converted
 		return df
 
+
 	def _convert_numerics(self, df: pd.DataFrame, columns: list[str]) -> pd.DataFrame:
 		for col in columns:
 			if col not in df.columns:
@@ -57,4 +64,21 @@ class TransformerExcel:
 				logger.warning(f"{nulls} valores numéricos inválidos na coluna '{col}'")
 
 			df[col] = converted
+		return df
+	
+
+	def _convert_booleans(self, df: pd.DataFrame, columns: list[str]) -> pd.DataFrame:
+		mapping = {"VERDADEIRO": True, "FALSO": False, "Ativo": True, "Inativo": False}
+		
+		for col in columns:
+			if col not in df.columns:
+				logger.warning(f"Coluna '{col}' não encontrada no DataFrame")
+				continue
+
+			if df[col].dtype == bool:
+				logger.info(f"Coluna '{col}' já é bool, pulando conversão")
+				continue
+
+			df[col] = df[col].map(mapping)
+    
 		return df
--- main.py
+++ main.py
@@ -13,11 +13,13 @@ data_departaments = extractor_departaments.extract()
 # transformer
 schemas = {
     'employees': DataFrameSchema(
-        date_columns=['data_admissao']
+        date_columns=['data_admissao'],
+		data_booleans=['ativo']
     ),
     'departments': DataFrameSchema(
         date_columns=['data_criacao'],
-        numeric_columns=['orcamento_anual']
+        numeric_columns=['orcamento_anual'],
+		data_booleans=['status']
     )
 }
 
@@ -32,6 +34,7 @@ df_employees_transformed = result['employees']
 df_departments_transformed = result['departments']
 
 
-print(df_employees_transformed.head(), df_departments_transformed.head())
+# print(df_employees_transformed.head(), df_departments_transformed.head())
+print(df_employees_transformed.info(), df_departments_transformed.info())
 
 
